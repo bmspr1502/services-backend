12ae401a6e2b64774c685c1a68a4ebc9066c5354
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2020-08-03 13:51:19 +0530

    updated services.sql

--- services.sql
+++ services.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 02, 2020 at 05:29 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.8
+-- Generation Time: Aug 03, 2020 at 10:19 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,15 +30,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `contact` (
   `id` int(100) NOT NULL,
-  `name` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `email` varchar(30) NOT NULL,
   `phone` varchar(10) NOT NULL,
-  `product` varchar(30) NOT NULL,
+  `product` varchar(50) NOT NULL,
   `brand` varchar(30) NOT NULL,
-  `description` varchar(200) NOT NULL,
+  `description` text NOT NULL,
   `youtube` varchar(50) NOT NULL,
-  `website` varchar(30) NOT NULL
+  `website` varchar(100) NOT NULL,
+  `visibility` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `phone`, `product`, `brand`, `description`, `youtube`, `website`, `visibility`) VALUES
+(1, 'PRANAVA RAMAN B M S', 'bmspr1502@gmail.com', '9443501317', 'Thin and Light Laptop @₹39,990', 'Lenovo Ideapad S540', '15.6 inches\r\n8GB RAM\r\ni5 8th gen\r\n512 GB SSD\r\n2GB Dedicated Graphics', 'https://www.youtube.com/watch?v=d4nHHXMlmnI', 'https://www.lenovo.com/in/en/l', 1),
+(2, 'ABC', 'abcd@gmail.com', '1234567890', 'Random thing', 'Random Brand', 'This is just a random thing that\\\'s being sold for a random price', 'https://www.youtube.com/watch?v=hY7m5jjJ9mM', 'https://cat-bounce.com/', 1);
 
 --
 -- Indexes for dumped tables
@@ -58,7 +68,7 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
