2322bb1f0dc6992191653d148a906b716e8a8c79
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2020-08-08 00:41:06 +0530

    Update aboutLeftChange.php

--- admin/about/aboutLeftChange.php
+++ admin/about/aboutLeftChange.php
@@ -6,6 +6,7 @@ $query="UPDATE `aboutus` SET `leftcontent`='$left' WHERE 1";
 $result = $con->query($query);
 if($result){
     echo stripslashes($left);
+    setcookie('about', '', time()-3600, '/');
 }
 else{
     echo "unsuccess";
